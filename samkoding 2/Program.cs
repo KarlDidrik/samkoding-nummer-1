e1b3a442565bb0f3ebca9e0b3b4631a99346be9a
KarlDidrik <karldodrik@gmail.com>
2023-11-24 14:30:34 +0100

    Add project files.

---
+++ samkoding 2/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace samkoding_2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gi kjæledyret ditt et navn: ");
+            string navn = Console.ReadLine();
+
+            Console.WriteLine($"Hvor gammel er {navn}");
+            int alder = Convert.ToInt32(Console.ReadLine());
+            var vpet = new pikachu(navn, alder);
+            Console.WriteLine($"Navn ={vpet.navn} og alder {vpet.alder}");
+
+            while (true)
+            {
+                Console.WriteLine("Velg en handling");
+                Console.WriteLine("1. Gi mat");
+                Console.WriteLine("2. Kos med dyret");
+                Console.WriteLine("3.  Sjekk toalettbehov");
+                Console.WriteLine("4. Sjekk hvordan dyret ditt har det");
+
+                string valg = Console.ReadLine();
+
+
+
+            switch (valg)
+            {
+                case "1":
+                    vpet.mat();
+                    break;
+                case "2":
+                    vpet.kos();
+                    break;
+                case "3":
+                    vpet.toalettsjekk();
+                    break;
+                    case "4":
+                        vpet.tilstand();
+                        break;
+            }
+            }
+        }
+    }
+}
---
+++ samkoding 2/Vpet.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace samkoding_2
+{
+    
+
+    internal class pikachu
+    {
+        public string navn;
+        public int alder;
+        public int sultenhet;
+        public int lykkelighet;
+        public int toalettbehov;
+
+        public pikachu(string navn, int alder, int sultenhet = 50, int lykkelighet = 50, int toalettbehov = 50)
+        {
+            this.navn = navn;
+            this.alder = alder;
+            this.sultenhet = sultenhet;
+            this.lykkelighet = lykkelighet;
+            this.toalettbehov = toalettbehov;
+        }
+        
+        public void mat()
+        {
+            sultenhet += 10;
+            lykkelighet += 5;
+            toalettbehov -= 5;
+            Console.WriteLine($"{navn} har fått mat!");
+        }
+        public void kos()
+        {
+            lykkelighet += 10;
+            sultenhet -= 5;
+            Console.WriteLine($"{navn} har fått kos!");
+        }
+
+        public void toalettsjekk()
+        {
+            if (toalettbehov < 60)
+            {
+                Console.WriteLine($"{navn} må på do snart!");
+            }
+            else
+            {
+                Console.WriteLine($"{navn} må ikke på do ennå!");
+            }
+        }
+        public void tilstand()
+        {
+            Console.WriteLine($"Sultenhet er {sultenhet} Lykkelighet er {lykkelighet} Toalettbehov er {toalettbehov}");
+        }
+
+
+    }
+    
+   
+}
